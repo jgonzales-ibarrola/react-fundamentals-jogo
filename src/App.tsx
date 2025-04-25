4844d6b14bef914ccd2cd4695ee79f1352a91626
jgonzales-ibarrola <jgonzales.ibarrola@gmail.com>
2025-04-25 15:11:01 +0800

    reset for creating CRUD operations

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import ComponentA from "./components/ComponentA";
-
 export default function App() {
   return (
     <div>
-      <ComponentA title="Component A" content="Test Content" />
+      
     </div>
   )
 }
